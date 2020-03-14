6c8788ff843e817d695036d6eec9dd2670990263
LelsersLasers <48894200+LelsersLasers@users.noreply.github.com>
2020-03-14 11:08:24 -0700

    working on set 4 of modal management

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,4 +1,12 @@
+import java.util.ArrayList;
 
 public class ObjectManager {
-
+	Rocketship rocketshipTwo;
+	
+	ArrayList<Projectile> shotsList = new ArrayList<Projectile>();
+	ArrayList<Alien> alienList = new ArrayList<Alien>();
+	
+	ObjectManager(Rocketship rocketshipTwo){
+		this.rocketshipTwo = rocketshipTwo;
+	}
 }
