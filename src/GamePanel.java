b74bba27855038e66085b402a9c85d03f3f548e9
LelsersLasers <48894200+LelsersLasers@users.noreply.github.com>
2020-03-16 19:57:39 -0700

    on step 5 of collision managment
    :five:

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,7 +5,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -20,8 +23,11 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	Font writtingFont;
 	
 	Timer frameDraw;
+	Timer alienSpawn;
 	
 	Rocketship rockship = new Rocketship(250,700, 50, 50);
+	
+	ObjectManager objectManager = new ObjectManager(rockship);
 	
 	GamePanel (){
 	    titleFont = new Font("Arial", Font.PLAIN, 48);
@@ -29,10 +35,20 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	    
 	    frameDraw = new Timer(1000/60, this);
 	    frameDraw.start();
+	    
+	}
+	
+	void startGame() {
+		alienSpawn = new Timer(1000 , objectManager);
+	    alienSpawn.start();
 	}
 	
 	void updateMenuState() {  }
-	void updateGameState() {  }
+	
+	void updateGameState() { 
+		objectManager.update();
+	}
+	
 	void updateEndState()  {  }
 	
 	void drawMenuState(Graphics g) {
@@ -48,10 +64,18 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 		g.drawString("Press SPACE for info", 125, 425);
 	}
 	void drawGameState(Graphics g) {
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		//g.setColor(Color.BLACK);
+		//g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		BufferedImage image = null;
+		try {
+			image = ImageIO.read(this.getClass().getResourceAsStream("space.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		g.drawImage(image, 0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT, null);
 		
-		rockship.draw(g);
+		objectManager.draw(g);
 	}
 	void drawEndState(Graphics g)  {
 		g.setColor(Color.RED);
@@ -101,8 +125,19 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 		if (e.getKeyCode()==KeyEvent.VK_ENTER) {
 		    if (currentState == END) {
 		        currentState = MENU;
-		    } else {
+		        alienSpawn.stop();
+		    } 
+		    if (currentState == MENU) {
+		    	currentState = GAME;
+		    	startGame();
+		    }
+		    else {
 		        currentState++;
+		    }
+		}
+		if (e.getKeyCode()==KeyEvent.VK_SPACE) {
+		    if (currentState == GAME) {
+		    	objectManager.addProjectile(rockship.getProjectile());
 		    }
 		}
 		if (e.getKeyCode()==KeyEvent.VK_UP) {
--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,12 +1,69 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Random;
 
-public class ObjectManager {
+public class ObjectManager implements ActionListener {
 	Rocketship rocketshipTwo;
 	
-	ArrayList<Projectile> shotsList = new ArrayList<Projectile>();
-	ArrayList<Alien> alienList = new ArrayList<Alien>();
+	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+	ArrayList<Alien> aliens = new ArrayList<Alien>();
+	Random random = new Random();
 	
 	ObjectManager(Rocketship rocketshipTwo){
 		this.rocketshipTwo = rocketshipTwo;
+	}
+	
+	void addProjectile(Projectile p) {
+		projectiles.add(p);
+	}
+	
+	void draw(Graphics g) {
+		rocketshipTwo.draw(g);
+		for (int i = 0; i < projectiles.size(); i++) {
+			projectiles.get(i).draw(g);
+		}
+		for (int i = 0; i < aliens.size(); i++) {
+			aliens.get(i).draw(g);
+		}
+	}
+	
+	void purgeObjects() {
+		for (int i = 0; i < projectiles.size(); i++) {
+			if (projectiles.get(i).isActive == false) {
+				projectiles.remove(i);
+			}
+		}
+		for (int i = 0; i < aliens.size(); i++) {
+			if (aliens.get(i).isActive == false) {
+				aliens.remove(i);
+			}
+		}
+	}
+	
+	void addAlien() {
+		aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
+	}
+	
+	void update(){
+		for (int i = 0; i < aliens.size(); i++) {
+			aliens.get(i).update();
+			if (aliens.get(i).y > LeagueInvaders.HEIGHT) {
+				aliens.get(i).isActive = false;
+			}
+		}
+		for (int i = 0; i < projectiles.size(); i++) {
+			projectiles.get(i).update();
+			if (projectiles.get(i).y < 0) {
+				projectiles.get(i).isActive = false;
+			}
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		addAlien();
 	}
 }
